8142d9d749ad014d889b8c0e96b56ccdc0b78c02
unknown <hh4518@gmail.com>
2021-07-26 14:39:37 +0900

    채팅 룸 생성

--- server/index.js
+++ server/index.js
@@ -4,8 +4,29 @@ const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('We have a new connection');
+
+  socket.on('join', ({ name, room }) => {
+    console.log(name, room);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User had left!!!');
+  });
+});
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`server has started on port ${PORT}`));
